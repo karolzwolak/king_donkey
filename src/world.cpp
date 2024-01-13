c62d2e1006a16b1ac20155d5cfdbea676efceb7b
Karol Zwolak <karolzwolak7@gmail.com>
2024-01-13 19:00:55 +0100

    feat: add levels tiles and ladders

--- src/world.cpp
+++ src/world.cpp
@@ -1,45 +1,33 @@
 #include "world.hpp"
 #include "objects.hpp"
+#include "screen.hpp"
 #include "texture.hpp"
+#include <cassert>
 
 World::World(int w, int h, TextureManager &textures)
     : width(w), height(h), player(Vector2(0, 0), NULL), tiles(NULL),
       tile_count(0), ladders(NULL), ladder_count(0), barrels(NULL),
-      barrel_count(0) {
+      barrel_count(0), textures(textures) {
 
   player = Player(Vector2(0, 0), &textures.player_texture);
 
-  tiles = new Tile[100];
+  tiles = new Tile[MAX_TILE_COUNT];
   tile_count = 0;
 
-  ladders = new Ladder[100];
+  ladders = new Ladder[MAX_LADDER_COUNT];
   ladder_count = 0;
-  barrels = new Barrel[100];
+  barrels = new Barrel[MAX_LADDER_COUNT];
   barrel_count = 0;
 
-  tiles[tile_count++] = Tile(Vector2(0, 0), &textures.tile_texture);
-  tiles[tile_count++] = Tile(Vector2(2, 2), &textures.tile_texture);
-  tiles[tile_count++] = Tile(Vector2(10, 10), &textures.tile_texture);
-  tiles[tile_count++] = Tile(Vector2(20, 20), &textures.tile_texture);
-  tiles[tile_count++] =
-      Tile(Vector2(640 - 16, 360 - 8), &textures.tile_texture);
+  gen_tile_line(1, 1, 2, -0.5);
+  gen_tile_line(1, 4, 14, 1.25, -1, 2);
+  gen_tile_line(4, 0, 18, -0.25, -1, 7);
+  gen_tile_line(5, 2, 28, 0.5, 9, 4);
 
-  int x = 0;
-  int y = 200;
-  for (int i = 0; i < 10; i++) {
-    tiles[tile_count++] = Tile(Vector2(x, y), &textures.tile_texture);
-    x += TILE_WIDTH;
-    y -= CLIMB_THRESHOLD;
-  }
+  gen_tile_line(3, 20, 32, 0.5, 5, 2);
+  gen_tile_line(26, 4, 32, -1.5, 6, 2);
 
-  for (int i = 0; i < 10; i++) {
-    tiles[tile_count++] = Tile(Vector2(x, y), &textures.tile_texture);
-    x += TILE_WIDTH;
-  }
-
-  ladders[ladder_count++] =
-      Ladder(Vector2(100, 100), 10, &textures.ladder_texture);
-  tiles[tile_count++] = Tile(Vector2(100, 100), &textures.tile_texture);
+  gen_tile_line(0, 5, 38, 0);
 
   barrels[barrel_count++] =
       Barrel(Vector2(60, 80), DIR_RIGHT, &textures.barrel_texture);
@@ -51,6 +39,32 @@ World::~World() {
   delete[] tiles;
   delete[] ladders;
   delete[] barrels;
+}
+void World::gen_tile_line(int skip_front, int skip_back, int tiles_from_bot,
+                          double dy, int ladder_on_id, int ladder_parts) {
+  int len = LOGICAL_SCREEN_WIDTH / TILE_WIDTH - skip_front - skip_back;
+  assert(tile_count + len < MAX_TILE_COUNT);
+
+  double x = TILE_WIDTH * skip_front;
+  double y = LOGICAL_SCREEN_HEIGHT - TILE_HEIGHT * tiles_from_bot;
+
+  if (ladder_parts > 0) {
+    if (ladder_on_id < 0)
+      ladder_on_id = len + ladder_on_id;
+
+    assert(ladder_count < MAX_LADDER_COUNT);
+    double ladder_x = x + ladder_on_id * TILE_WIDTH + LADDER_WIDTH / 2.;
+    double ladder_y = y + +dy * ladder_on_id;
+
+    ladders[ladder_count++] = Ladder(Vector2(ladder_x, ladder_y), ladder_parts,
+                                     &textures.ladder_texture);
+  }
+
+  for (int i = 0; i < len; i++) {
+    tiles[tile_count++] = Tile(Vector2(x, y), &textures.tile_texture);
+    x += TILE_WIDTH;
+    y += dy;
+  }
 }
 
 Ladder *World::intersecting_ladder(DynamicObject *obj) {
--- src/world.hpp
+++ src/world.hpp
@@ -4,6 +4,10 @@
 #include "screen.hpp"
 #include "texture.hpp"
 
+#define MAX_BARREL_COUNT 20
+#define MAX_TILE_COUNT 300
+#define MAX_LADDER_COUNT 20
+
 class World {
 
 public:
@@ -19,10 +23,14 @@ public:
   Barrel *barrels;
   int barrel_count;
 
+  TextureManager &textures;
+
   World(int w, int h, TextureManager &textures);
   ~World();
 
   void update(double dt);
+  void gen_tile_line(int skip_front, int skip_back, int tiles_from_bot,
+                     double dy, int ladder_on_id = 0, int ladder_parts = 0);
   Ladder *intersecting_ladder(DynamicObject *obj);
   void draw(Screen &screen);
 };
